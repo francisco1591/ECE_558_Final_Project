2c561cc6a0ad154f9585a0343f7946ca57bb08ba
Francisco1591 <ihaSari13>
2017-06-10 13:06:09 -0700

    Testing commit on Francisco machine

--- app/src/main/java/edu/pdx/ece558/grp4/remotephonecontrol/SMSListener.java
+++ app/src/main/java/edu/pdx/ece558/grp4/remotephonecontrol/SMSListener.java
@@ -71,7 +71,7 @@ public class SMSListener extends Service {
             final Bundle bundle = intent.getExtras();
 
             try {
-
+                // test ccomment
                 if (bundle != null) {
 
                     final Object[] pdusObj = (Object[]) bundle.get("pdus");
